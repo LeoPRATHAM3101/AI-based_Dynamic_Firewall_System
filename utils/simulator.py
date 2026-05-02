2002cde7628f143e5f1fbe6c0fa0001abd460932
Pratham Joshi <129934064+LeoPRATHAM3101@users.noreply.github.com>
2026-05-02 21:20:43 +0530

    Update simulator.py

--- utils/simulator.py
+++ utils/simulator.py
@@ -1,1 +1,13 @@
+import random
+from utils.logger import log_attack
 
+def simulate_attack():
+    fake_ip = f"192.168.1.{random.randint(1,255)}"
+    threat_score = random.randint(50, 100)
+
+    log_attack(fake_ip, threat_score)
+
+    return {
+        "ip": fake_ip,
+        "threat": threat_score
+    }
