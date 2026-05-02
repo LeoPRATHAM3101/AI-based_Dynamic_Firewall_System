5d275d87e08cd68e204c5439e47066d19e9ed503
Pratham Joshi <129934064+LeoPRATHAM3101@users.noreply.github.com>
2026-05-02 21:29:26 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,23 +1,37 @@
 import json
 from datetime import datetime
+from utils.geo import get_ip_location
 
 LOG_FILE = "logs.json"
 
 
 def log_attack(ip, threat_score):
-    log = {
+    # Get geo info
+    geo = get_ip_location(ip)
+
+    # Create full log entry
+    log_entry = {
         "ip": ip,
         "time": str(datetime.now()),
-        "threat_score": threat_score
+        "threat_score": threat_score,
+        "country": geo["country"],
+        "region": geo["region"],
+        "city": geo["city"],
+        "lat": geo["lat"],
+        "lon": geo["lon"],
+        "isp": geo["isp"]
     }
 
+    # Load existing logs safely
     try:
         with open(LOG_FILE, "r") as f:
             data = json.load(f)
     except:
         data = []
 
-    data.append(log)
+    # Append new log
+    data.append(log_entry)
 
+    # Save back to file
     with open(LOG_FILE, "w") as f:
         json.dump(data, f, indent=4)
