8b9bbeec06127d58ae717f7a2251bbcde8eca453
Pratham Joshi <129934064+LeoPRATHAM3101@users.noreply.github.com>
2026-05-02 21:21:03 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,23 @@
+import json
+from datetime import datetime
 
+LOG_FILE = "logs.json"
+
+
+def log_attack(ip, threat_score):
+    log = {
+        "ip": ip,
+        "time": str(datetime.now()),
+        "threat_score": threat_score
+    }
+
+    try:
+        with open(LOG_FILE, "r") as f:
+            data = json.load(f)
+    except:
+        data = []
+
+    data.append(log)
+
+    with open(LOG_FILE, "w") as f:
+        json.dump(data, f, indent=4)
